262472b4917b57a95f763d55a44ac37a4bd3561c
Bengsiswanto <mightiesthero@gmail.com>
2019-04-09 00:40:09 +0700

    refactoring

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def test_run():
-  # Define date range
-  start_date = "2019-01-01"
-  end_date = "2019-04-01"
-  dates = pd.date_range(start_date, end_date)
+def symbol_to_path(symbol, base_dir="data"):
+  # Return CSV path given ticker symbol
+  return os.path.join(base_dir, "{}.csv".format(str(symbol)))
 
-  # Create an empty dataframe
-  df1 = pd.DataFrame(index = dates)
+def get_data(symbols, dates):
+  # Read stock data (adjusted close) for given symbols from CSV files
+  df = pd.DataFrame(index = dates)
 
-  # Read symbols data into temporary dataframe
-  symbols = ["BBRI.JK", "INDF.JK"]
   for symbol in symbols:
-    df_temp = pd.read_csv("data/{}.csv".format(symbol),
+    df_temp = pd.read_csv(symbol_to_path(symbol),
       index_col = "Date",
       parse_dates = True,
       usecols=["Date", "Adj Close"],
@@ -23,9 +21,21 @@ def test_run():
     df_temp = df_temp.rename(columns={"Adj Close": symbol})
 
     # Join the two dataframes using DataFrame.join()
-    df1 = df1.join(df_temp, how = "inner")
+    df = df.join(df_temp, how = "inner")
 
-  print(df1)
+  return df
+
+def test_run():
+  # Define date range
+  start_date = "2019-01-01"
+  end_date = "2019-04-01"
+  dates = pd.date_range(start_date, end_date)
+
+  symbols = ["BBRI.JK", "INDF.JK"]
+
+  df = get_data(symbols, dates)
+
+  print(df)
 
 if __name__ == "__main__":
   test_run()

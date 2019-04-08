518c0b9008ba1e647ea38230f9863ff6a84bf36c
Bengsiswanto <mightiesthero@gmail.com>
2019-04-08 23:40:59 +0700

    plot adj close

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import pandas as pd
-
-def get_max_close(symbol):
-  df = pd.read_csv("data/{}.csv".format(symbol))
-  return df["Close"].max()
-
-def get_mean_volume(symbol):
-  df = pd.read_csv("data/{}.csv".format(symbol))
-  return df["Volume"].mean()
+import matplotlib.pyplot as plt
 
 def test_run():
-  for symbol in ["BBRI.JK" ,"INDF.JK"]:
-    print("Mean Volume")
-    print(symbol, get_mean_volume(symbol))
+  df = pd.read_csv("data/INDF.JK.csv")
+  print(df["Adj Close"])
+  df["Adj Close"].plot()
+  plt.show()
 
 if __name__ == "__main__":
   test_run()

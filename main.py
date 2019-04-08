4f61ffbc6cfd74c841a7b129bd569a3d3f1836d2
Bengsiswanto <mightiesthero@gmail.com>
2019-04-09 00:05:03 +0700

    generate dates and create data frame with dates as index

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def test_run():
-  df = pd.read_csv("data/INDF.JK.csv")
-  df[["Close", "Adj Close"]].plot()
-  plt.show()
+  start_date = "2018-01-01"
+  end_date = "2018-01-30"
+  dates = pd.date_range(start_date, end_date)
+  df1 = pd.DataFrame(index = dates)
 
 if __name__ == "__main__":
   test_run()

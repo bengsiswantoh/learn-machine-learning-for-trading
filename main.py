d2b9f27f7f6a7d909d610253217a77231453f1b5
Bengsiswanto <mightiesthero@gmail.com>
2019-04-08 22:39:27 +0700

    get mean volume and print

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ def get_max_close(symbol):
   df = pd.read_csv("data/{}.csv".format(symbol))
   return df["Close"].max()
 
+def get_mean_volume(symbol):
+  df = pd.read_csv("data/{}.csv".format(symbol))
+  return df["Volume"].mean()
+
 def test_run():
   for symbol in ["BBRI.JK" ,"INDF.JK"]:
-    print("Max Close")
-    print(symbol, get_max_close(symbol))
+    print("Mean Volume")
+    print(symbol, get_mean_volume(symbol))
 
 if __name__ == "__main__":
   test_run()

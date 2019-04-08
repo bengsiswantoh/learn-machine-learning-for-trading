1ca60ef810f743814af6f60e17c349f90d4e3526
Bengsiswanto <mightiesthero@gmail.com>
2019-04-09 00:32:04 +0700

    combine several data

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ def test_run():
   # Create an empty dataframe
   df1 = pd.DataFrame(index = dates)
 
-  # Read INDF data into temporary dataframe
-  dfINDF = pd.read_csv("data/INDF.JK.csv",
-    index_col = "Date",
-    parse_dates = True,
-    usecols=["Date", "Adj Close"],
-    na_values=["nan"])
+  # Read symbols data into temporary dataframe
+  symbols = ["BBRI.JK", "INDF.JK"]
+  for symbol in symbols:
+    df_temp = pd.read_csv("data/{}.csv".format(symbol),
+      index_col = "Date",
+      parse_dates = True,
+      usecols=["Date", "Adj Close"],
+      na_values=["nan"])
 
-  # Join the two dataframes using DataFrame.join()
-  df1 = df1.join(dfINDF, how = "inner")
+    # Rename columns to symbol
+    df_temp = df_temp.rename(columns={"Adj Close": symbol})
 
-  # Drop NaN Values
+    # Join the two dataframes using DataFrame.join()
+    df1 = df1.join(df_temp, how = "inner")
+
   print(df1)
 
 if __name__ == "__main__":

c3d88b755d16e969c0f0545941e5a164b27b36a7
Bengsiswanto Hendrawan <mightiesthero@gmail.com>
2019-04-09 16:42:16 +0700

    timing python operations

--- num.py
+++ num.py
@@ -1,15 +1,10 @@
-import numpy as np
-
-def get_max_index(a):
-  return a.argmax()
+import time
 
 def test_run():
-  a = np.array([9, 6, 2, 3, 12, 14, 7, 10], dtype=np.int32)  # 32-bit integer array
-  print("Array:", a)
-
-  # Find the maximum and its index in array
-  print("Maximum value:", a.max())
-  print("Index of max.:", get_max_index(a))
+  t1 = time.time()
+  print("ML4T")
+  t2 = time.time()
+  print("The time taken by print statement is ", t2 - t1, " seconds")
 
 if __name__ == "__main__":
   test_run()

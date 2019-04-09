860cbb0de4720cd0b4b0d97eece77d22d861a08f
Bengsiswanto Hendrawan <mightiesthero@gmail.com>
2019-04-09 16:38:57 +0700

    quiz

--- num.py
+++ num.py
@@ -1,23 +1,15 @@
 import numpy as np
 
+def get_max_index(a):
+  return a.argmax()
+
 def test_run():
-  a = np.random.seed(693) # seed the random number generator
-  a = np.random.randint(0, 10, size = (5, 4))
-  print("Array:\n", a)
+  a = np.array([9, 6, 2, 3, 12, 14, 7, 10], dtype=np.int32)  # 32-bit integer array
+  print("Array:", a)
 
-  # Sum of all elements
-  print("Sum of all elements:", a.sum())
-
-  # Iterate over rows, to compute sum of each column
-  print("Sum of each column:\n", a.sum(axis = 0))
-
-  # Iterate over columns, to compute sum of each row
-  print("Sum of each row:\n", a.sum(axis = 1))
-
-  # Statistics: min, max, mean (across rows, cols, and overall)
-  print("Minimum of each column:\n", a.min(axis = 0))
-  print("Maximum of each row:\n", a.max(axis = 1))
-  print("Mean of all elements:\n", a.mean())
+  # Find the maximum and its index in array
+  print("Maximum value:", a.max())
+  print("Index of max.:", get_max_index(a))
 
 if __name__ == "__main__":
   test_run()

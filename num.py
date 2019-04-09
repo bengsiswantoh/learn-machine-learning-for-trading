68305ffc92db2e84d0f0981d3d462f04cc2f310b
Bengsiswanto Hendrawan <mightiesthero@gmail.com>
2019-04-09 14:44:30 +0700

    refactoring

--- num.py
+++ num.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def test_run():
   # Generate an array full of random numbers
-  print(np.random.random((5, 4))) # pass in a size tuple
+  print(np.random.rand(5, 4))
 
 if __name__ == "__main__":
   test_run()

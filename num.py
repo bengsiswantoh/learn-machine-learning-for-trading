126646b86254d149864bcbda4df04e85621e499d
Bengsiswanto Hendrawan <mightiesthero@gmail.com>
2019-04-09 16:00:43 +0700

    array attributes

--- num.py
+++ num.py
@@ -2,10 +2,14 @@ import numpy as np
 
 def test_run():
   # Random Integers
-  print(np.random.randint(10)) # a single integer in [0, 10]
-  print(np.random.randint(0, 10)) # same as above, specifying [low, high] explicit
-  print(np.random.randint(0, 10, size = 5)) # 5 random integer as a 1D array
-  print(np.random.randint(0, 10, size = (2, 3))) # 2x3 array of random integers
+  a = np.random.random((5, 4))
+  print(a.shape)
+  print(a.shape[0]) # number of rows
+  print(a.shape[1]) # number of columns
+  print(len(a.shape))
+  print(a.size)
+  print(a)
+  print(a.dtype)
 
 if __name__ == "__main__":
   test_run()

91ce3f8a2571bcd85359c4d683fc8b21e61fba52
Bengsiswanto Hendrawan <mightiesthero@gmail.com>
2019-04-09 11:36:35 +0700

    array with spesific datatype

--- num.py
+++ num.py
@@ -1,10 +1,8 @@
 import numpy as np
 
 def test_run():
-  # Empty array
-  print(np.empty(5))
-  print(np.empty((5,4)))
-  print(np.empty((5,4,3)))
+  # Array of one and datatype int
+  print(np.ones((5,4), dtype = np.int_))
 
 if __name__ == "__main__":
   test_run()

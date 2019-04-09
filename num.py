7609c13b187656b15e557ba72cafb0abc7e1d9a1
Bengsiswanto Hendrawan <mightiesthero@gmail.com>
2019-04-09 17:20:10 +0700

    slicing

--- num.py
+++ num.py
@@ -1,10 +1,20 @@
 import time
+import numpy as np
 
 def test_run():
-  t1 = time.time()
-  print("ML4T")
-  t2 = time.time()
-  print("The time taken by print statement is ", t2 - t1, " seconds")
+  a = np.random.rand(5, 4)
+  print("Array:\n", a)
+
+  # Accessing element at position (3, 2)
+  element = a[3, 2]
+  print(element)
+
+  print(a[0, 1:3])
+
+  print(a[0:2, 0:2])
+
+  # Slice start:stop:step
+  print(a[:, 0:3:2]) # will select columns 0, 2 for every row
 
 if __name__ == "__main__":
   test_run()

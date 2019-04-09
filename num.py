b482d93837a3e00d25dacc18224fc817c0ccb525
Bengsiswanto Hendrawan <mightiesthero@gmail.com>
2019-04-09 14:50:30 +0700

    change mean to 50 and s.d. to 10

--- num.py
+++ num.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def test_run():
   # sample numbers from Gaussian (normal) distribution
-  print(np.random.normal(size = (2, 3)))
+  print(np.random.normal(50, 10, size = (2, 3))) # change mean to 50 and s.d. to 10
 
 if __name__ == "__main__":
   test_run()

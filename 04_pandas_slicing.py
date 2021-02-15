59c806cd0ee4c2dfceb95c0b0de1f8474995d0c6
Bengsiswanto <bengsiswantoh@gmail.com>
2021-02-15 14:30:42 +0700

    fix error syntax

--- 04_pandas_slicing.py
+++ 04_pandas_slicing.py
@@ -1,57 +1,62 @@
 import os
 import pandas as pd
 
+
 def symbol_to_path(symbol, base_dir="data"):
-  # Return CSV path given ticker symbol
-  return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+    # Return CSV path given ticker symbol
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
 
 def get_data(symbols, dates):
-  # Read stock data (adjusted close) for given symbols from CSV files
-  df = pd.DataFrame(index = dates)
+    # Read stock data (adjusted close) for given symbols from CSV files
+    df = pd.DataFrame(index=dates)
 
-  for symbol in symbols:
-    df_temp = pd.read_csv(symbol_to_path(symbol),
-      index_col = "Date",
-      parse_dates = True,
-      usecols=["Date", "Adj Close"],
-      na_values=["nan"])
+    for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol),
+                              index_col="Date",
+                              parse_dates=True,
+                              usecols=["Date", "Adj Close"],
+                              na_values=["nan"])
 
-    # Rename columns to symbol
-    df_temp = df_temp.rename(columns={"Adj Close": symbol})
+        # Rename columns to symbol
+        df_temp = df_temp.rename(columns={"Adj Close": symbol})
 
-    # Join the two dataframes using DataFrame.join()
-    df = df.join(df_temp, how = "inner")
+        # Join the two dataframes using DataFrame.join()
+        df = df.join(df_temp, how="inner")
 
-  return df
+    return df
 
-def combine_several_data(show = False):
-  symbols = ["BBRI.JK", "INDF.JK"]
 
-  start_date = "2018-01-01"
-  end_date = "2018-02-01"
+def combine_several_data(show=False):
+    symbols = ["BBCA.JK", "BBRI.JK"]
 
-  dates = pd.date_range(start_date, end_date)
+    start_date = "2020-01-01"
+    end_date = "2020-02-01"
 
-  df = get_data(symbols, dates)
+    dates = pd.date_range(start_date, end_date)
 
-  if show:
-    print(df)
+    df = get_data(symbols, dates)
 
-  return df
+    if show:
+        print(df)
+
+    return df
+
 
 def run():
-  # combine_several_data(True)
+    # combine_several_data(True)
 
-  df = combine_several_data()
-  # Slice by row range (dates) using DataFrame.ix[] selector
-  print(df.ix["2018-01-01":"2018-01-10"])
+    df = combine_several_data()
+    # Slice by row range (dates) using DataFrame.ix[] selector
+    print(df.loc["2020-01-01":"2020-01-10"])
 
-  # Slice by column (symbols)
-  print(df["INDF.JK"])
-  print(df[["BBRI.JK", "INDF.JK"]])
+    # Slice by column (symbols)
+    print(df["BBCA.JK"])
+    print(df[["BBCA.JK", "BBRI.JK"]])
 
-  # Slice by row and column
-  print(df.ix["2018-01-01":"2018-01-10", ["BBRI.JK", "INDF.JK"]])
+    # # Slice by row and column
+    print(df.loc["2020-01-01":"2020-01-10", ["BBCA.JK", "BBRI.JK"]])
+
 
 if __name__ == "__main__":
-  run()
+    run()

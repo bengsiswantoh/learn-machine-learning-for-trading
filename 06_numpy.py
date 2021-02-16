9677ed867dfad23177f5d9ce54c84989b4191240
Bengsiswanto <bengsiswantoh@gmail.com>
2021-02-16 11:00:00 +0700

    add comments

--- 06_numpy.py
+++ 06_numpy.py
@@ -1,53 +1,76 @@
+# https://numpy.org/doc/stable/reference/random/index.html
+
 import numpy as np
 
+
 def create_1D_array():
-  print(np.array([2, 3, 4]))
+    print(np.array([2, 3, 4]))
+
 
 def create_2D_array():
-  print(np.array([(2, 3, 4), (5, 6, 7)]))
+    print(np.array([(2, 3, 4), (5, 6, 7)]))
+
 
 def create_1D_empty_array():
-  print(np.empty(5))
+    print(np.empty(5))
+
 
 def create_2D_empty_array():
-  print(np.empty((5, 4)))
+    print(np.empty((5, 4)))
+
 
 def create_3D_empty_array():
-  print(np.empty((5, 4, 3)))
+    print(np.empty((5, 4, 3)))
+
 
 def create_array_specific_datatype():
-  print(np.ones((5, 4), dtype = np.int_))
+    print(np.ones((5, 4), dtype=np.int_))
+
 
 def create_array_random_values():
-  print(np.random.random((5, 4)))
+    print(np.random.random((5, 4)))
+
 
 def create_array_random_values_alt():
-  print(np.random.rand(5, 4))
+    print(np.random.rand(5, 4))
+
 
 def create_array_random_gaussian():
-  print(np.random.normal(size = (2, 3)))
+    print(np.random.normal(size=(2, 3)))
+
 
 def create_array_random_gaussian_specific():
-  print(np.random.normal(50, 10, size = (2, 3))) # change mean to 50 and s.d. to 10
+    # change mean to 50 and s.d. to 10
+    print(np.random.normal(50, 10, size=(2, 3)))
+
 
 def random_integers():
-  print(np.random.randint(10)) # a single integer in [0, 10]
-  print(np.random.randint(0, 10)) # same as above, specifying [low, high] explicit
-  print(np.random.randint(0, 10, size = 5)) # 5 random integer as a 1D array
-  print(np.random.randint(0, 10, size = (2, 3))) # 2x3 array of random integers
+    # a single integer in [0, 10]
+    print(np.random.randint(10))
+
+    # same as above, specifying [low, high] explicit
+    print(np.random.randint(0, 10))
+
+    # 5 random integer as a 1D array
+    print(np.random.randint(0, 10, size=5))
+
+    # 2x3 array of random integers
+    print(np.random.randint(0, 10, size=(2, 3)))
+
 
 def run():
-  create_1D_array()
-  create_2D_array()
-  create_1D_empty_array()
-  create_2D_empty_array()
-  create_3D_empty_array()
-  create_array_specific_datatype()
-  create_array_random_values()
-  create_array_random_values_alt()
-  create_array_random_gaussian()
-  create_array_random_gaussian_specific()
-  random_integers()
+    create_1D_array()
+    create_2D_array()
+    create_1D_empty_array()
+    create_2D_empty_array()
+    create_3D_empty_array()
+    create_array_specific_datatype()
+    create_array_random_values()
+    create_array_random_values_alt()
+    create_array_random_gaussian()
+    create_array_random_gaussian_specific()
+    random_integers()
+
 
 if __name__ == "__main__":
-  run()
+    run()
--- 07_numpy_functions.py
+++ 07_numpy_functions.py
@@ -1,42 +1,43 @@
 import numpy as np
 
+
 def run():
-  # Random Integers
-  a = np.random.random((5, 4))
-  print(a.shape)
-  print(a.shape[0]) # number of rows
-  print(a.shape[1]) # number of columns
-  print(len(a.shape))
-  print(a.size)
-  print(a)
-  print(a.dtype)
+    # Random Integers
+    a = np.random.random((5, 4))
+    print(a.shape)
+    print(a.shape[0])  # number of rows
+    print(a.shape[1])  # number of columns
+    print(len(a.shape))  # dimension in an array
+    print(a.size)  # total number of elements in an array
+    print(a)
+    print(a.dtype)
 
+    a = np.random.seed(693)  # seed the random number generator
+    a = np.random.randint(0, 10, size=(5, 4))
+    print("Array:\n", a)
 
-  a = np.random.seed(693) # seed the random number generator
-  a = np.random.randint(0, 10, size = (5, 4))
-  print("Array:\n", a)
+    # Sum of all elements
+    print("Sum of all elements:", a.sum())
 
-  # Sum of all elements
-  print("Sum of all elements:", a.sum())
+    # Iterate over rows, to compute sum of each column
+    print("Sum of each column:\n", a.sum(axis=0))
 
-  # Iterate over rows, to compute sum of each column
-  print("Sum of each column:\n", a.sum(axis = 0))
+    # Iterate over columns, to compute sum of each row
+    print("Sum of each row:\n", a.sum(axis=1))
 
-  # Iterate over columns, to compute sum of each row
-  print("Sum of each row:\n", a.sum(axis = 1))
+    # Statistics: min, max, mean (across rows, cols, and overall)
+    print("Minimum of each column:\n", a.min(axis=0))
+    print("Maximum of each row:\n", a.max(axis=1))
+    print("Mean of all elements:\n", a.mean())
 
-  # Statistics: min, max, mean (across rows, cols, and overall)
-  print("Minimum of each column:\n", a.min(axis = 0))
-  print("Maximum of each row:\n", a.max(axis = 1))
-  print("Mean of all elements:\n", a.mean())
+    a = np.array([9, 6, 2, 3, 12, 14, 7, 10],
+                 dtype=np.int32)  # 32-bit integer array
+    print("Array:", a)
 
+    # Find the maximum and its index in array
+    print("Maximum value:", a.max())
+    print("Index of max.:", a.argmax())
 
-  a = np.array([9, 6, 2, 3, 12, 14, 7, 10], dtype=np.int32)  # 32-bit integer array
-  print("Array:", a)
-
-  # Find the maximum and its index in array
-  print("Maximum value:", a.max())
-  print("Index of max.:", a.argmax())
 
 if __name__ == "__main__":
-  run()
+    run()
